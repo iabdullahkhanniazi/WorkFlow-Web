55c6a3751a98421aaa8e2b67b82bb808ae0330c8
Abdullah Khan Niazi <abdullahkhan3811@gmail.com>
2025-07-11 11:08:59 +0500

    Deploy Fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This line tells your app its correct path on GitHub Pages
-  base: '/taskflow-app/', // Make sure this matches your repository name
+  // This path MUST exactly match your GitHub repository name
+  base: '/WorkFlow-Web/', 
   plugins: [react()],
 })
